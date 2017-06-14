dfeb2719204fac70095d80c6ded450e9467cd297
david gray <davidseangray@gmail.com>
2017-06-14 16:33:57 +0100

    draw functionality added

--- lib/game.rb
+++ lib/game.rb
@@ -6,9 +6,11 @@ class Game
     @player1 = true
     @board = board.grid
     @possible_wins = [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+    @turns = 0
   end
 
   def play(choice)
+    @turns += 1
     selection = choice - 1
     @player1 == true ? @board[selection] = "O" : @board[selection] = "X"
     change_player
@@ -35,6 +37,13 @@ class Game
       count = 0
       win.each { |square| count += 1 if @board[square] == "X"}
       return "Well done! You win!" if count == 3
+    end
+    check_draw
+  end
+
+  def check_draw
+    if @turns == 9
+      return "No Winners here! It's a Draw..."
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,7 @@ describe Game do
   subject(:game) { described_class.new }
 
   describe 'Player 1' do
-    it "is gets the first choice on the grid" do
+    it "Gets the first choice on the grid" do
       expect(game.player1).to eq(true)
     end
   end
@@ -19,7 +19,7 @@ describe Game do
       expect(game.player1).to eq(false)
     end
 
-    it "when player 1 makes a choice, player 2 can place an X" do
+    it "When player 1 makes a choice, player 2 can place an X" do
       game.play(1)
       game.play(2)
       expect(game.board).to eq(["O","X","_","_","_","_","_","_","_"])
@@ -27,11 +27,11 @@ describe Game do
   end
 
   describe "Game Logic" do
-    it "has 8 possible winning lines" do
+    it "Has 8 possible winning lines" do
       expect(game.possible_wins).to eq([[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]])
     end
 
-    it "allows player O to win" do
+    it "Allows player O to win" do
       game.play(1)
       game.play(5)
       game.play(2)
@@ -40,7 +40,7 @@ describe Game do
       expect(game.board).to eq(["O","O","O","_","X","_","_","_","X"])
     end
 
-    it "allows player X to win" do
+    it "Allows player X to win" do
       game.play(5)
       game.play(1)
       game.play(9)
@@ -48,6 +48,19 @@ describe Game do
       game.play(4)
       expect(game.play(3)).to eq("Well done! You win!")
       expect(game.board).to eq(["X","X","X","O","O","_","_","_","O"])
+    end
+
+    it "Ends the game if there is a draw and no spaces are left." do
+      game.play(1)
+      game.play(2)
+      game.play(3)
+      game.play(5)
+      game.play(4)
+      game.play(7)
+      game.play(8)
+      game.play(9)
+      expect(game.play(6)).to eq("No Winners here! It's a Draw...")
+      expect(game.board).not_to include("_")
     end
   end
 end
