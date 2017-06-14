1937a4c6c17d9acbe19628cf9fc4e66f322c486c
david gray <davidseangray@gmail.com>
2017-06-14 14:16:55 +0100

    has 8 possible win lines in the array

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,11 @@
 class Game
 
-  attr_reader :player1, :board
+  attr_reader :player1, :board, :possible_wins
 
   def initialize(board = Board.new)
     @player1 = true
     @board = board.grid
+    @possible_wins = [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
   end
 
   def play(choice)
@@ -13,8 +14,12 @@ class Game
     change_player
   end
 
+  private
+
   def change_player
     @player1 = !@player1
   end
+
+
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -25,4 +25,10 @@ describe Game do
       expect(game.board).to eq(["O","X","_","_","_","_","_","_","_"])
     end
   end
+
+  describe "Game Logic" do
+    it "has 8 possible winning lines" do
+      expect(game.possible_wins).to eq([[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]])
+    end
+  end
 end
